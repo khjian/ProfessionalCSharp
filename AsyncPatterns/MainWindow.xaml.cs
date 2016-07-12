43fe1de193f9a5b039e244cee5a970b5775734b4
khjian <khjian@KHJIAN-PC>
2016-07-12 22:20:02 +0800

    异步模式

--- AsyncPatterns/MainWindow.xaml.cs
+++ AsyncPatterns/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using AsyncLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,12 @@ namespace AsyncPatterns
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SearchInfo searchInfo;
         public MainWindow()
         {
             InitializeComponent();
+            searchInfo = new SearchInfo();
+            this.DataContext = searchInfo;
         }
 
         private void OnClear(object sender, RoutedEventArgs e)
@@ -32,12 +37,40 @@ namespace AsyncPatterns
 
         private void OnSearchSync(object sender, RoutedEventArgs e)
         {
-
+            foreach (var req in GetSearchRequests())
+            {
+                var client = new WebClient();
+                client.Credentials = req.Credentials;
+                string resp = client.DownloadString(req.Url);
+                IEnumerable<SearchItemResult> images = req.Parse(resp);
+                foreach (var image in images)
+                {
+                    searchInfo.List.Add(image);
+                }
+            }
         }
 
         private void OnSeachAsyncPattern(object sender, RoutedEventArgs e)
         {
-
+            Func<string, ICredentials, string> downloadString = (address, cred) =>
+            {
+                var client = new WebClient();
+                client.Credentials = cred;
+                return client.DownloadString(address);
+            };
+            Action<SearchItemResult> addItem = item => searchInfo.List.Add(item);
+            foreach (var req in GetSearchRequests())
+            {
+                downloadString.BeginInvoke(req.Url, req.Credentials, ar =>
+                {
+                    string resp = downloadString.EndInvoke(ar);
+                    IEnumerable<SearchItemResult> images = req.Parse(resp);
+                    foreach (var image in images)
+                    {
+                        this.Dispatcher.Invoke(addItem, image);
+                    }
+                }, null);
+            }
         }
 
         private void OnAsyncEventPattern(object sender, RoutedEventArgs e)
@@ -53,6 +86,15 @@ namespace AsyncPatterns
         private void OnCancel(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private IEnumerable<IImageRequest> GetSearchRequests()
+        {
+            return new List<IImageRequest>
+            {
+                new BingRequest() {SearchTerm = searchInfo.SearchTerm},
+                //new FlickrRequest() {SearchTerm = searchInfo.SearchTerm}
+            };
         }
     }
 }
