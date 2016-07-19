8087dbd03ac6371fe7d8d76c8dbd63e3df11c4d8
songshujian <kuaihujian@qq.com>
2016-07-19 17:56:18 +0800

    单元测试

---
+++ Demo/DeepThought.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class DeepThought
+    {
+        public int TheAnswer()
+        {
+            return 42;
+        }
+    }
+}
---
+++ DemoTests/DeepThoughtTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Tests
+{
+    [TestClass]
+    public class DeepThoughtTests
+    {
+        [TestMethod]
+        public void TheAnswerTest()
+        {
+            int expected = 42;
+            DeepThought f1 = new DeepThought();
+            int actual = f1.TheAnswer();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
