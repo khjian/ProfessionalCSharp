1e82d63b7e677cc91dd19cd659bf46524b9a6890
songshujian <kuaihujian@qq.com>
2016-07-20 18:24:43 +0800

    完成单元测试Demo

---
+++ Demo/StringSample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Demo
+{
+    public class StringSample
+    {
+        public StringSample(string init)
+        {
+            if(init == null)
+                throw new ArgumentNullException("init");
+            this.init = init;
+        }
+        private string init;
+
+        public string GetStringDemo(string first, string second)
+        {
+            if (first == null)
+                throw new ArgumentNullException("first");
+            if(string.IsNullOrEmpty(first))
+                throw new ArgumentNullException("empty string is not allowed",first);
+            if(second == null)
+                throw  new ArgumentNullException("second");
+            if(second.Length > first.Length)
+                throw new ArgumentOutOfRangeException("second","must be shorter than firs");
+
+            int startIndex = first.IndexOf(second);
+            if (startIndex < 0)
+            {
+                return $"{second} not found in {first}";
+            }
+            else if(startIndex<5)
+            {
+                return $"removed {second} from {first}:{first.Remove(startIndex, second.Length)}";
+            }
+            else
+            {
+                return init.ToUpperInvariant();
+            }
+        }
+    }
+}
---
+++ DemoTests/StringSampleTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Demo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Tests
+{
+    [TestClass()]
+    public class StringSampleTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestStringSampleNull()
+        {
+            StringSample sample = new StringSample(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetStringDemoFirstNull()
+        {
+            StringSample sample = new StringSample(string.Empty);
+            string actual = sample.GetStringDemo(null, "");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetStringDemoFirstEmpty()
+        {
+            StringSample sample = new StringSample(string.Empty);
+            string actual = sample.GetStringDemo("", "a");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetStringDemoSecondNull()
+        {
+            StringSample sample = new StringSample(string.Empty);
+            string actual = sample.GetStringDemo("a", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void GetStringDemoOutRange()
+        {
+            StringSample sample = new StringSample(string.Empty);
+            string actual = sample.GetStringDemo("a", "aa");
+        }
+
+        [TestMethod]
+        public void TestStringSampleIsNotNull()
+        {
+            StringSample sample = new StringSample("a");
+        }
+
+        [TestMethod]
+        public void GetStringDemoAB()
+        {
+            string expected = "b not found in a";
+            StringSample sample = new StringSample(string.Empty);
+            string actual = sample.GetStringDemo("a", "b");
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void GetStringDemoABCDBC()
+        {
+            string expected = "removed bc from abcd:ad";
+            StringSample sample = new StringSample(string.Empty);
+            string actual = sample.GetStringDemo("abcd", "bc");
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void GetStringDemoTestToUpper()
+        {
+            string expected = "TEST";
+            StringSample sample = new StringSample("Test");
+            string actual = sample.GetStringDemo("aaaaaac", "c");
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
