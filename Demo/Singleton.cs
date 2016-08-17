e65c2a1e997b1b8e03191dcbc940cf88a04477f6
khjian <khjian@KHJIAN-PC>
2016-08-17 22:28:42 +0800

    单例模式，双锁机制

---
+++ Demo/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public sealed class Singleton
+    {
+        private static Singleton myInstance;
+        private static readonly object lockRoot = new object();
+        private Singleton()
+        { }
+
+        public static Singleton GetInstance()
+        {
+            if (myInstance == null)
+            {
+                lock (lockRoot)
+                {
+                    if (myInstance == null)
+                    {
+                        myInstance = new Singleton();
+                    }
+                }
+            }
+            return myInstance;
+        }
+    }
+}
