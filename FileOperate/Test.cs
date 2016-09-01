a4927538485ebca07e615726357edd83d112ae98
khjian <khjian@KHJIAN-PC>
2016-09-01 22:32:40 +0800

    文件操作

---
+++ FileOperate/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperate
+{
+    public class Test
+    {
+        public void ForTest()
+        { 
+            File.Copy(@"C:\1.txt",@"D:\copy.txt");
+
+            FileInfo myFile = new FileInfo(@"C:\1.txt");
+            myFile.CopyTo(@"D:\copy.txt");
+
+            Console.WriteLine(Path.Combine(@"C:\My Documents","ReadMe.txt"));
+        }
+    }
+}
