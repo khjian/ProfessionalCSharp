802cd0a1c49f3173d3ddab023f7b9e18dfd6883c
Song <Administrator@SONG>
2016-09-03 18:17:10 +0800

    注册表操作

--- FileOperate/Test.cs
+++ FileOperate/Test.cs
@@ -4,19 +4,28 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace FileOperate
 {
     public class Test
     {
         public void ForTest()
-        { 
-            File.Copy(@"C:\1.txt",@"D:\copy.txt");
+        {
+            File.Copy(@"C:\1.txt", @"D:\copy.txt");
 
             FileInfo myFile = new FileInfo(@"C:\1.txt");
+            StreamReader sr = myFile.OpenText();
             myFile.CopyTo(@"D:\copy.txt");
 
-            Console.WriteLine(Path.Combine(@"C:\My Documents","ReadMe.txt"));
+            StreamReader sr2 = new StreamReader(@"C:\My Documents\ReadMe.txt");
+
+            FileStream fs = new FileStream(@"C:\My Documents\ReadMe.txt", FileMode.Open, FileAccess.Read, FileShare.None);
+            StreamReader sr3 = new StreamReader(fs);
+
+            RegistryKey hklm = Registry.LocalMachine;
+            RegistryKey hkSofeware = hklm.OpenSubKey("Software");
+            RegistryKey hkMicrosoft = hkSofeware.CreateSubKey("MyOwnSoftware");
         }
     }
 }
